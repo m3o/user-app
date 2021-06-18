62bb51439b1b344ebeec8a82f38a7835686cbed4
Janos Dobronszki <dobronszki@gmail.com>
2021-06-18 12:02:33 +0100

    F

--- user-app/src/app/app.module.ts
+++ user-app/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
