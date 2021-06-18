29bfe68f35b8b771ec1edc460b5e494ea1604f01
Janos Dobronszki <dobronszki@gmail.com>
2021-06-18 09:26:11 +0100

    F

--- user-app/src/app/app.component.ts
+++ user-app/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { ActivatedRoute } from "@angular/router";
+
+const verifyEndpoint =
+  "https://angry-cori-854281.netlify.app/.netlify/functions/verify";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+  redirectUrl = "";
+  token = "";
+
+  constructor(private http: HttpClient, private router: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.router.paramMap.subscribe((paramMap) => {
+      this.token = paramMap.get("token");
+      if (!this.token) {
+        alert("missing token");
+        return;
+      }
+      this.redirectUrl = paramMap.get("redirectUrl");
+      if (!this.redirectUrl) {
+        alert("missing redirect url");
+        return;
+      }
+
+      this.http
+        .post(verifyEndpoint, { token: this.token })
+        .toPromise()
+        .then(() => {
+          window.location.href = this.redirectUrl;
+        })
+        .catch((e) => {
+          alert(e);
+        });
+    });
+  }
 }
