aa18cc88b44bde6387be9d0b3e44cd2b022a5e1b
Janos Dobronszki <dobronszki@gmail.com>
2021-06-18 09:27:32 +0100

    F

--- netlify-functions/verify.js
+++ netlify-functions/verify.js
@@ -3,11 +3,10 @@ const { API_KEY } = process.env;
 const m3o = require('@m3o/m3o-node');
 
 exports.handler = async function (event, context) {
-  let response = await new m3o.Client({ token: API_KEY }).call(
-    'file',
-    'List',
-    {}
-  );
+  let body = JSON.parse(event.body);
+  let response = await new m3o.Client({ token: API_KEY }).call('user', 'Verify', {
+    token: body.token,
+  });
   return {
     statusCode: 200,
     body: JSON.stringify(response),
