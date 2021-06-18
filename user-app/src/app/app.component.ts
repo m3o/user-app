4c03ad35f98c6ea89e6335754a9c5dd50792ea03
Janos Dobronszki <dobronszki@gmail.com>
2021-06-18 15:14:49 +0100

    F

--- user-app/src/app/app.component.ts
+++ user-app/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { ActivatedRoute } from "@angular/router";
 const verifyEndpoint =
   "https://angry-cori-854281.netlify.app/.netlify/functions/verify";
 
-  
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -23,12 +22,12 @@ export class AppComponent {
     this.router.paramMap.subscribe((paramMap) => {
       this.token = paramMap.get("token");
       if (!this.token) {
-        alert("missing token");
+        console.log("missing token");
         return;
       }
       this.redirectUrl = paramMap.get("redirectUrl");
       if (!this.redirectUrl) {
-        alert("missing redirect url");
+        console.log("missing redirect url");
         return;
       }
 
