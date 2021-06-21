d38a0ccd97d572a29ad991d2d46d9a0e9e15056d
Janos Dobronszki <dobronszki@gmail.com>
2021-06-21 12:17:19 +0100

    F

--- user-app/netlify-functions/verify.js
+++ user-app/netlify-functions/verify.js
@@ -1,14 +1,39 @@
 const { API_KEY } = process.env;
 
-const m3o = require('@m3o/m3o-node');
+const m3o = require("@m3o/m3o-node");
 
 exports.handler = async function (event, context) {
+  if (!API_KEY) {
+    return {
+      statusCode: 500,
+      body: { error: "no api key" },
+    };
+  }
+  
   let body = JSON.parse(event.body);
-  let response = await new m3o.Client({ token: API_KEY }).call('user', 'VerifyEmail', {
-    token: body.token,
-  });
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response),
-  };
+  if (!body.token) {
+    return {
+      statusCode: 400,
+      body: { error: "no token" },
+    };
+  }
+  try {
+    let response = await new m3o.Client({ token: API_KEY }).call(
+      "user",
+      "VerifyEmail",
+      {
+        token: body.token,
+      }
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      statusCode: 500,
+      body: { error: JSON.stringify(e) },
+    };
+  }
 };
