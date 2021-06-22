8f29290a30f938bb4c59b0689bdc494206b7901a
Janos Dobronszki <dobronszki@gmail.com>
2021-06-22 16:06:28 +0100

    Handle failure case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,37 @@
-import { Component } from "@angular/core";
-import { FormControl } from "@angular/forms";
-import { HttpClient } from "@angular/common/http";
-import { ActivatedRoute } from "@angular/router";
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 const verifyEndpoint =
-  "https://angry-cori-854281.netlify.app/.netlify/functions/verify";
+  'https://angry-cori-854281.netlify.app/.netlify/functions/verify';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = "app";
-  redirectUrl = "";
-  token = "";
+  title = 'app';
+  redirectUrl = '';
+  failureRedirectUrl = '';
+  token = '';
 
   constructor(private http: HttpClient, private router: ActivatedRoute) {}
 
   ngOnInit() {
     this.router.queryParams.subscribe((paramMap) => {
-      this.token = paramMap["token"];
+      this.token = paramMap['token'];
       if (!this.token) {
-        console.log("missing token");
+        console.log('missing token');
         return;
       }
-      this.redirectUrl = paramMap["redirectUrl"];
+      this.redirectUrl = paramMap['redirectUrl'];
       if (!this.redirectUrl) {
-        console.log("missing redirect url");
+        console.log('missing redirect url');
         return;
       }
+      this.failureRedirectUrl = paramMap['failureRedirectUrl'];
 
       this.http
         .post(verifyEndpoint, { token: this.token })
@@ -38,7 +40,11 @@ export class AppComponent {
           window.location.href = this.redirectUrl;
         })
         .catch((e) => {
-          console.log(e)
+          console.log(e);
+          if (this.failureRedirectUrl) {
+            window.location.href = this.failureRedirectUrl;
+            return;
+          }
           alert(JSON.stringify(e));
         });
     });
